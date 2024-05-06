3a486ebbe401792f9a6912cfe3f0daefdf5e55e9
Bruno Silva <brunosilva9725@gmail.com>
2024-05-06 16:34:40 -0300

    Erro CORS

--- Joks/src/main/java/com/WorkJoks/Joks/JoksApplication.java
+++ Joks/src/main/java/com/WorkJoks/Joks/JoksApplication.java
@@ -2,6 +2,9 @@ package com.WorkJoks.Joks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JoksApplication {
@@ -10,4 +13,17 @@ public class JoksApplication {
 		SpringApplication.run(JoksApplication.class, args);
 	}
 
-}
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/clientes/**")
+                        .allowedOrigins("http://localhost:8080/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+	}
+	}
